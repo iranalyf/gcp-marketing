6a646170e658d2044cf910da0e3dfc3d3b273ea8
Iran Ályf <iranalyf2222@gmail.com>
2018-12-02 23:23:53 -0200

    Update main.py

--- main.py
+++ main.py
@@ -3,10 +3,26 @@
 from logzero import logger
 import uuid
 import json
+import requests
+
+
+def send_notification_from_function():
+    try:
+        request_call_function = requests.post("https://us-central1-pocprojetcintegratorfinal.cloudfunctions.net/notification").json()
+        logger.info("calling from function notification")
+        data_function = {
+            "status_code": request_call_function.status_code,
+            "data": json.loads(request_call_function.text)
+        }
+        return json.dumps(data_function)
+    except Exception as ex:
+        logger.warning(str(ex))
+        return
 
 
 if __name__ == '__main__':
-    user_id = uuid.uuid4()
-    logger.info('consumer data from Report Users, promotion available, now send function to notificated USER {}'.format(user_id))
+    call = uuid.uuid4()
+    logger.info('consumer data from Report Users, promotion available, now send function to notificated USER {}'.format(call))
+    send_notification_from_function()
 
 
